47d93957e7d32a62d368a42877e052de4c9e0698
Travis Reed <travis.reed@webfilings.com>
2016-08-04 19:37:47 -0500

    making progress

---
+++ src/game/game.py
@@ -0,0 +1,18 @@
+from google.appengine.ext import ndb
+from google.appengine.ext.ndb import polymodel
+
+
+class Game(polymodel.PolyModel):
+    date = ndb.DateTimeProperty(auto_now_add=True)
+    players = ndb.KeyProperty("User", repeated=True)
+    winners = ndb.KeyProperty("User", repeated=True)
+
+class PlayerPoolBallsState(ndb.StructuredProperty):
+    player = ndb.KeyProperty("user")
+    balls_remaining = ndb.IntegerProperty()
+
+
+class PoolGame(Game):
+    final_state = ndb.StructuredProperty(PlayerPoolBallsState, repeated=True)
+
+
