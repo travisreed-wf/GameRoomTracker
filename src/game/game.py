dde467896fb793abbb10caedbf5c089105c0e95d
Travis Reed <travis.reed@webfilings.com>
2016-08-04 19:42:57 -0500

    a frew more classes

--- src/game/game.py
+++ src/game/game.py
@@ -5,14 +5,27 @@ from google.appengine.ext.ndb import polymodel
 class Game(polymodel.PolyModel):
     date = ndb.DateTimeProperty(auto_now_add=True)
     players = ndb.KeyProperty("User", repeated=True)
+    point_data = ndb.StructuredProperty(PointData, repeated=True)
     winners = ndb.KeyProperty("User", repeated=True)
 
+
 class PlayerPoolBallsState(ndb.StructuredProperty):
-    player = ndb.KeyProperty("user")
+    player = ndb.KeyProperty("User")
     balls_remaining = ndb.IntegerProperty()
+
+
+class PointData(ndb.StructuredProperty):
+    player = ndb.KeyProperty("User")
+    points = ndb.IntegerProperty()
 
 
 class PoolGame(Game):
     final_state = ndb.StructuredProperty(PlayerPoolBallsState, repeated=True)
 
 
+class CutThroatGame(PoolGame):
+    pass
+
+
+class EightBallGame(PoolGame):
+    pass
