56233e546307fbfc27028056fbc9b818e3a9749e
Joseph McGovern <joseph.mcgovern@webfilings.com>
2016-08-06 12:37:54 -0500

    update home page

--- src/home/views.py
+++ src/home/views.py
@@ -7,6 +7,6 @@ class HomeView(MethodView):
     def get(self):
         return render_template('home.html')
 
+
 def setup_urls(app):
     app.add_url_rule('/', view_func=HomeView.as_view('home'))
-
