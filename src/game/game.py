72b5b6e7c2907d1eb84ecb302989fe4d9491fb35
Travis Reed <travis.reed@webfilings.com>
2016-08-06 13:31:13 -0500

    8 ball implementation

--- src/game/game.py
+++ src/game/game.py
@@ -9,3 +9,19 @@ class Game(polymodel.PolyModel):
     player_record_keys = ndb.KeyProperty(
         kind="GamePlayerRecord", repeated=True, required=True)
     winner_keys = ndb.KeyProperty(kind="User", repeated=True)
+
+    @staticmethod
+    def _update_ranked_data(sorted_player_records, rated_rating_groups):
+        to_put = [sorted_player_records]
+        for team in rated_rating_groups:
+            record_index = 0
+            for rating in team.itervalues():
+                record_index += 1
+                record = sorted_player_records[record_index]
+                player = record.player
+                points_earned = rating.mu - player.rank_data.points
+                record.rank_points_earned = points_earned
+                player.rank_data.points = rating.mu
+                player.rank_data.elasticity = rating.sigma
+                to_put.append(player)
+        ndb.put_multi(to_put)
--- src/game/pool_game.py
+++ src/game/pool_game.py
@@ -16,41 +16,20 @@ class PoolGame(Game):
         :returns: a game object
         """
         game = cls(player_records=[r.key for r in player_records])
-        game.calculate_experience(player_records)
-        game.calculate_rank_points_changes(player_records)
+        game.calculate_experience()
+        game.calculate_rank_points_changes()
         game.players = [s.player for s in player_records]
         game.put()
         return game  #  TODO: Calculate winner
 
-    def calculate_rank_points_changes(self, player_records):
+    def calculate_rank_points_changes(self):
         """
         Calculate points using self.player_records
         Should be overwritten by subclass
         """
-        player_records = sorted(
-            player_records, key=lambda r: r.player_placement)
-        teams = [{r: r.player.rating} for r in player_records]
+        pass
 
-        ranks = [r.player_placement for r in player_records]
-
-        env = TrueSkill(
-            mu=DEFAULT_RANK_POINTS, sigma=DEFAULT_RANK_ELASTICITY,
-            draw_probability=0.0)
-
-        to_put = []
-        rated_rating_groups = env.rate(teams, ranks=ranks)
-        for team in rated_rating_groups:
-            for record, rating in team.iteritems():
-                player = record.player
-                points_earned = rating.mu - player.rank_data.points
-                record.rank_points_earned = points_earned
-                player.rank_data.points = rating.mu
-                player.rank_data.elasticity = rating.sigma
-                to_put.append(player)
-
-        ndb.put_multi(to_put)
-
-    def calculate_experience(self, player_records):
+    def calculate_experience(self):
         """
         Use self.player_records to assign experience to the relevant players
         """
@@ -60,19 +39,48 @@ class PoolGame(Game):
 
 class CutThroatGame(PoolGame):
 
-    def calculate_rank_points_changes(self, player_records):
+    def calculate_rank_points_changes(self):
         """
         Calculate Points using the following formula
         A win is worth 5 points, second place is worth 2 points
         """
-        pass  # TODO
+        player_records = ndb.get_multi(self.player_record_keys)
+        player_records = sorted(
+            player_records, key=lambda r: r.player_placement)
+        teams = [{r.player.name: r.player.rating} for r in player_records]
+
+        ranks = [r.player_placement for r in player_records]
+
+        env = TrueSkill(
+            mu=DEFAULT_RANK_POINTS, sigma=DEFAULT_RANK_ELASTICITY,
+            draw_probability=0.0)
+        rated_rating_groups = env.rate(teams, ranks=ranks)
+        self._update_ranked_data(player_records, rated_rating_groups)
 
 
 class EightBallGame(PoolGame):
 
-    def calculate_rank_points_changes(self, player_records):
+    def calculate_rank_points_changes(self):
         """
         Calculate Points using the following formula
         A win is worth 3 points
         """
-        pass  # TODO
+        player_records = ndb.get_multi(self.player_record_keys)
+        team1 = [r for r in player_records if r.player_placement == 1]
+        team2 = [r for r in player_records if r.player_placement == 2]
+        player_records = sorted(
+            player_records, key=lambda r: r.player_placement)
+        teams = []
+        for team_records in [team1, team2]:
+            team = {}
+            for record in team_records:
+                team[record.player.name] = record.player.rating
+            teams.append(team)
+
+        ranks = [0, 1]
+
+        env = TrueSkill(
+            mu=DEFAULT_RANK_POINTS, sigma=DEFAULT_RANK_ELASTICITY,
+            draw_probability=0.0)
+        rated_rating_groups = env.rate(teams, ranks=ranks)
+        self._update_ranked_data(player_records, rated_rating_groups)
