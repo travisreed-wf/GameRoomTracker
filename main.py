8a2570dfc23e2cb199b2650e030eb459c205aa37
Joseph McGovern <joseph.mcgovern@webfilings.com>
2016-08-08 21:27:32 -0500

    remove toolbar

--- main.py
+++ main.py
@@ -11,8 +11,6 @@ app.debug = True
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 app.secret_key = "SecretKey"
 
-# toolbar = DebugToolbarExtension(app)
-
 game_api.setup_urls(app)
 home_views.setup_urls(app)
 user_api.setup_urls(app)
